8f17bbf5a29ccec12fb969870da3a88b59b06b69
Ivan Hahanov <ivanhahanov13@gmail.com>
2022-03-20 23:17:51 +0300

    refactor

--- main.py
+++ main.py
@@ -1,109 +1,137 @@
-from multiprocessing import BoundedSemaphore
+import os
 import telebot
 import time
-import strings
-import user
+from user import Users, User
 from telebot import types
+from bot_logging import logger
 
-bot = telebot.TeleBot("")
-users = {}
-@bot.message_handler(commands=['start', 'help'])
+token = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    messagetoedit = bot.send_message(chat_id = message.chat.id, text="Загрузка 0%"+ '\n' + "▒"*(10))
-    time.sleep(0.25)
-    for i in range(1,11):
-                bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Загрузка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                time.sleep(0.1)
-    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text=strings.hello_message)
-    bot.send_message(chat_id = message.chat.id, text=strings.first_message)
+    db = Users()
+    user = User()
+    user.chat_id = message.chat.id
+    user.status = 1
+    user.username = message.from_user.username
+    if db.users.get(user.chat_id):
+        logger.info(f"update exists user: {user.username} {user.chat_id}")
+    else:
+        logger.info(f"add new user: {user.username} {user.chat_id}")
+    db.users[user.chat_id] = user
+    db.save()
 
-    users[message.chat.id] = user.user_model(message.chat.id)
-    users[message.chat.id].status = 1
-    print(users)
+    loading_widget(message.chat.id, "Давай познакомимся\nСкажи мне свои фамилию и имя.")
+
+
+def loading_widget(chat_id, next_message):
+    message_to_edit = bot.send_message(chat_id=chat_id,
+                                       text=f"Обработка 0%\n{'▒' * 10}")
+    time.sleep(0.25)
+    for i in [1, 5, 9]:
+        bot.edit_message_text(chat_id=chat_id,
+                              message_id=message_to_edit.message_id,
+                              text=f"Обработка {i * 10}%\n{'█' * i}{'▒' * (10 - i)}")
+        time.sleep(0.25)
+    bot.edit_message_text(chat_id=chat_id,
+                          message_id=message_to_edit.message_id,
+                          text=next_message)
+
+
+def get_users_card(chat_id):
+    db = Users()
+    users_info = db.users.get(chat_id)
+    if users_info:
+        message = f"Давай проверим.\nТебя зовут {users_info.name}\nТы учишься в группе {users_info.group}"
+        if users_info.team:
+            message += f"\nТвоя команда называется {users_info.team}"
+            return message
+        message += "\nКоманды у тебя пока нет"
+        return message
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-      if(message.chat.id in users):
-                print(users[message.chat.id].status)
-                if(users[message.chat.id].status == 5):
-                                        users[message.chat.id].setifCommand(message.text)
-                                        users[message.chat.id].status = 10
-                
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, text="Обработка 0%"+ '\n' + "▒"*(10))
-                                        time.sleep(0.25)
-                                        for i in range(1,11):
-                                                    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Обработка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                                                    time.sleep(0.1)
-                                        bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text=strings.end_of_dialog)
-                                        users[message.chat.id].sendToFile()
-                                        return
-                if(users[message.chat.id].status == 4):
-                                    if(message.text == "да(манда)"):
-                                        users[message.chat.id].setStatusCommand(True)
-                                        users[message.chat.id].status = 5
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, reply_markup = types.ReplyKeyboardRemove (selective = False), text="Обработка 0%"+ '\n' + "▒"*(10))
-                                        bot.delete_message(message.chat.id, messagetoedit.message_id)
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, text="Обработка 0%"+ '\n' + "▒"*(10))
+    db = Users()
 
-                                        time.sleep(0.25)
-                                        for i in range(1,11):
-                                                    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Обработка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                                                    time.sleep(0.1)
-                                        bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text=strings.name_command)
-                                        return
-                                    if(message.text == "нет(пидора ответ)"):
-                                        users[message.chat.id].setStatusCommand(False)
-                                        users[message.chat.id].status = 10
+    chat_id = str(message.chat.id)
+    text = message.text
+    if chat_id in db.users:
+        if db.users[chat_id].status == 1:
+            db.users[chat_id].name = text
+            db.users[chat_id].status += 1
+            db.save()
+            logger.info(f"{chat_id}:{db.users[chat_id].username} add name: {text}")
+            bot.send_message(chat_id=message.chat.id, text='А какая у тебя группа?')
 
-                                        #TODO here we can save info about user in file
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, reply_markup = types.ReplyKeyboardRemove (selective = False), text="Обработка 0%"+ '\n' + "▒"*(10))
-                                        bot.delete_message(message.chat.id, messagetoedit.message_id)
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, text="Обработка 0%"+ '\n' + "▒"*(10))
-                                        time.sleep(0.25)
-                                        for i in range(1,11):
-                                                    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Обработка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                                                    time.sleep(0.1)
-                                        bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text=strings.end_of_dialog)
-                                        users[message.chat.id].sendToFile()
 
-                                        return
+        elif db.users[chat_id].status == 2:
+            db.users[chat_id].set_group(text)
+            db.users[chat_id].status += 1
+            db.save()
+            logger.info(f"{chat_id}:{db.users[chat_id].username} add group: {text}")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True).row(types.KeyboardButton('Да'),
+                                                                         types.KeyboardButton('Нет'))
+            bot.send_message(chat_id=message.chat.id, reply_markup=markup, text="У тебя есть команда?")
 
-                if(users[message.chat.id].status == 3):
-                                        users[message.chat.id].setGroup(message.text)
-                                        users[message.chat.id].status = 4
-                                        markup = types.ReplyKeyboardMarkup().row(types.KeyboardButton('да(манда)'), types.KeyboardButton('нет(пидора ответ)'))
+        elif db.users[chat_id].status == 3:
+            if text.lower() == "да":
+                db.users[chat_id].status += 1
+                bot.send_message(chat_id=message.chat.id,
+                                 text="А как называется?",
+                                 reply_markup=types.ReplyKeyboardRemove(selective=False))
+                db.save()
+            elif text.lower() == "нет":
+                db.users[chat_id].status = 5
+                db.users[chat_id].team = ''
+                db.save()
+                logger.info(f"{chat_id}:{db.users[chat_id].username} save empty team")
+                loading_widget(chat_id, get_users_card(chat_id))
 
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, text="Обработка 0%"+ '\n' + "▒"*(10))
-                                        time.sleep(0.25)
-                                        for i in range(1,11):
-                                                    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Обработка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                                                    time.sleep(0.1)
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True).row(types.KeyboardButton('Да'),
+                                                                             types.KeyboardButton('Нет'))
+                bot.send_message(chat_id=message.chat.id, reply_markup=markup, text='Всё верно?')
+            else:
+                bot.send_message(chat_id=message.chat.id, text='Я понимаю только бинарные ответы "Да" или "Нет"')
+        elif db.users[chat_id].status == 4:
+            db.users[chat_id].team = text
+            db.users[chat_id].status += 1
+            db.save()
+            logger.info(f"{chat_id}:{db.users[chat_id].username} add team: {text}")
+            loading_widget(chat_id, get_users_card(chat_id))
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True).row(types.KeyboardButton('Да'),
+                                                                         types.KeyboardButton('Нет'))
+            bot.send_message(chat_id=message.chat.id, reply_markup=markup, text='Всё верно?')
 
-                                        bot.delete_message(message.chat.id, messagetoedit.message_id)
-                                        bot.send_message(chat_id = message.chat.id,reply_markup = markup, text=strings.to_get_team)
-                                        
-                                        return
-                if(users[message.chat.id].status == 2):
-                                        users[message.chat.id].setGroup(message.text)
-                                        users[message.chat.id].status = 3
-                
-                                        messagetoedit = bot.send_message(chat_id = message.chat.id, text="Обработка 0%"+ '\n' + "▒"*(10))
-                                        time.sleep(0.25)
-                                        for i in range(1,11):
-                                                    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Обработка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                                                    time.sleep(0.1)
-                                        bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text=strings.third_message)
-                                        return
-                if(users[message.chat.id].status == 1):
-                        users[message.chat.id].setName(message.text)
-                        users[message.chat.id].status = 2
-  
-                        messagetoedit = bot.send_message(chat_id = message.chat.id, text="Обработка 0%"+ '\n' + "▒"*(10))
-                        time.sleep(0.25)
-                        for i in range(1,11):
-                                    bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text="Обработка " + str(i*10) +"%"+ '\n' + "█"*i + "▒"*(10-i))
-                                    time.sleep(0.25)
-                        bot.edit_message_text(chat_id=message.chat.id, message_id=messagetoedit.message_id, text=strings.second_message)
-                        return
+        elif db.users[chat_id].status == 5:
+            if text.lower() == "да":
+                db.users[chat_id].status = 10
+                text = "Поздравляю!\nЯ принял твою заявку, теперь ты можешь подписаться на канал <название канала>." \
+                       "\nТам я постараюсь сообщать полезную для тебя информацию."
+                if not db.users[chat_id].team:
+                    text += ", а в чате <название чата> ты сможешь собрать свою команду или присоединиться к кому-нибудь."
+                logger.info(f"{chat_id}:{db.users[chat_id].username} finish registration")
+                bot.send_message(chat_id=message.chat.id,
+                                 text=text,
+                                 reply_markup=types.ReplyKeyboardRemove(selective=False))
+                db.save()
+            elif text.lower() == "нет":
+                db.users[chat_id].status = 1
+                logger.info(f"{chat_id}:{db.users[chat_id].username} restart registration")
+                bot.send_message(chat_id=message.chat.id,
+                                 text="Давай попробуем ещё раз.\n\nСкажи мне свою фамилию и имя.",
+                                 reply_markup=types.ReplyKeyboardRemove(selective=False))
+                db.save()
+            else:
+                logger.warn(f"{chat_id}:{db.users[chat_id].username} unsupported answer: {text}")
+                bot.send_message(chat_id=message.chat.id, text='Я понимаю только бинарные ответы "Да" или "Нет"')
+        else:
+            logger.warn(f"{chat_id}:{db.users[chat_id].username} receive: {text}")
 
-bot.infinity_polling()
+
+if __name__ == '__main__':
+    logger.info("start bot")
+    bot.infinity_polling()
--- user.py
+++ user.py
@@ -1,20 +1,39 @@
 import json
-class user_model:
-        def __init__(self,chat_id):
-                self.FIO        = ""
-                self.group      = ""
-                self.isCommand  = False
-                self.ifCommand  = ""
-                self.status     = -1
-                self.chat_id    = chat_id
-        def setName(self, FIO):
-                self.FIO = FIO
-        def setGroup(self, group):
-                self.group = group
-        def setStatusCommand(self, isCommand):
-                self.isCommand = isCommand
-        def setifCommand(self, ifCommand):
-                self.ifCommand = ifCommand
-        def sendToFile(self):
-                with open('data.txt', 'a') as f:
-                    f.write(self.FIO + " " + self.group + " " +  str(self.isCommand) + " " + self.ifCommand + '\n')
+from pathlib import Path
+
+
+class User:
+    def __init__(self, name: str = "",
+                 group: str = "",
+                 team: str = "",
+                 status: int = -1,
+                 chat_id: int = -1,
+                 username: str = ""):
+        self.name = name
+        self.username = username
+        self.group = group
+        self.team = team
+        self.status = status
+        self.chat_id = chat_id
+
+    def set_group(self, group):
+        self.group = group
+
+
+class Users:
+    def __init__(self):
+        self.path = Path("users.json")
+        self.users = dict()
+        self.load_users()
+
+    def load_users(self):
+        if self.path.exists():
+            with self.path.open() as u:
+                users = json.loads(u.read())['users']
+                for key, value in users.items():
+                    self.users[key] = User(**value)
+
+    def save(self):
+        with self.path.open("w") as u:
+            users = {k: v.__dict__ for k, v in self.users.items()}
+            u.write(json.dumps({"users": users}, indent=2))
